cd5c943a76f53bfccf191f70e5b039cc7b1f5946
BugHunter-X <askzenai@gmail.com>
2025-01-27 04:32:07 +0100

    Add serverSolution.js file

---
+++ serverSolution.js
@@ -0,0 +1,23 @@
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const http = require('http');
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  const requestListener = (request, response) => {
+    response.writeHead(200);
+    response.end('Hello, World!');
+  };
+  const server = http.createServer(requestListener);
+  server.listen(8080);
+} 
